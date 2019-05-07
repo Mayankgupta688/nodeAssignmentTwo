1dc6a4d769133594c1122e4184ec6f1243ec21cf
Mayank Gupta (IBAM) <Mayank.Gupta@iontrading.com>
2019-05-07 13:55:52 +0530

    Employee List Displayed

--- server.js
+++ server.js
@@ -1,21 +1,14 @@
 var express = require("express");
-var fs = require("fs");
+var request = require('request');
 
 var app = express();
 
 app.set('view engine', 'ejs');
 
 app.get("/", function(req, res) {
-    fs.readFile("./employeeDetails.json", function(err, data) {
-        var employeeList = JSON.parse(data);
-        response.send(employeeList);
-    })
+    request.get('http://5c055de56b84ee00137d25a0.mockapi.io/api/v1/employees', function (error, response, body) {
+        res.render("index", {employeeList: JSON.parse(body)});
+    });
 });
-
-app.get("/employees", function(req, res) {
-    fs.readFile("./employeeDetails.json", function(err, data) {
-        res.render("index", {employeeList: JSON.parse(data).employeeDetails});
-    })
-})
 
 app.listen(8000);
